c1c5da78256fa284b050405d4bb6fe508f13f882
b1gmcdaddy <bobtangpuz@gmail.com>
2024-06-25 11:33:44 +0800

    added: fontawesome package

--- client/src/components/Banner.vue
+++ client/src/components/Banner.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="bg-[#0e1420] rounded-lg w-[80%] md:p-10 xs:p-5 flex mx-auto sm:mt-28 md:mt-36 shadow-neutral-400 shadow-md">
     <div class="flex justify-between items-center w-full">
-        <h1 class="md:text-4xl xs:text-xl tracking-wider font-bold text-[#f9c97e]">Jolo's Restos</h1>
+        <div class="flex items-center">
+            <span class="mr-6 "><font-awesome-icon icon="utensils" class="text-4xl text-[#f9c97e]" /></span>
+            <h1 class="md:text-4xl xs:text-xl tracking-wider font-bold text-[#f9c97e]">Jolo's Restos</h1>
+        </div>
         <h2 class="text-[#f9c97e] text-xl">v.4.0</h2>
     </div>
 </div>
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f132c7e6f116e10f6a39a8fde9cd892bf72656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14f0bd775f67815b4dbb1a73524241e3735f676")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
